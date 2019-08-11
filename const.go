91f8d4cd5a5883412be8ca1bc4c12526be2fdc2c
Albert Jin <albert.jin@gmail.com>
2019-08-11 09:52:42 +0800

    ec: remove const.go for strings and create errors.go to define standard errors

--- const.go
+++
@@ -1,4 +0,0 @@
-package el
-
-const PartiallyUnexpected = "partially unexpected\n%v"
-const Unexpected = "unexpected\n%v"
---
+++ errors.go
@@ -0,0 +1,7 @@
+package el
+
+import "errors"
+
+var (
+    ErrNil = errors.New("the pointer is unexpectedly nil")
+)
