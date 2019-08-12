5aeaf0aaea34e289c4b8d4950ce38e9ba5abc643
Albert Jin <albert.jin@gmail.com>
2019-08-12 23:58:03 +0800

    ec: add ErrPending

--- errors.go
+++ errors.go
@@ -5,5 +5,6 @@ import "errors"
 var (
 	ErrNil        = errors.New("the pointer is nil")
 	ErrUnexpected = errors.New("the value is unexpected")
+	ErrPending    = errors.New("the feature is pending")
 	ErrVoid       = errors.New("no descriptive information is available")
 )
